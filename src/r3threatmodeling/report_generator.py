21967c996e01751d1901e44e7e25d7f39f99720b
David Cervigni <david.cervigni@r3.com>
2023-07-12 13:38:42 +0100

    mermaid_include for MD

--- src/r3threatmodeling/report_generator.py
+++ src/r3threatmodeling/report_generator.py
@@ -226,6 +226,8 @@ def processSingleTMID(tmoRoot, TMID, args):
 
     mdReport = createRFIs(mdReport)
 
+    mdReport = "\n".join(process_mermaidInclude(mdReport.splitlines(), assetDir[1]))
+
 
     postProcessTemplateFile(outputDir, browserSync, mdOutFileName, htmlOutFileName, mdReport, assetDir)
     return
@@ -239,7 +241,9 @@ def postProcessTemplateFile(outputDir, browserSync, mdOutFileName, htmlOutFileNa
     mermaid_include = MermaidInclude(
     configs={'base_path': assetDir[1].__str__()}
 )
-    htmlReport = markdown.markdown(mermaidHtmlTags, extensions=['md_in_html', 'markdown_include.include', mermaid_include])
+    htmlReport = markdown.markdown(mermaidHtmlTags, extensions=['md_in_html'
+                                                                # , 'markdown_include.include', mermaid_include
+                                                                ])
         
     baseHTML = """<!DOCTYPE html>
         <html>
@@ -330,6 +334,101 @@ def createRFIs(mdData):
     return newstring.replace("__RFI_PLACEHOLDER__", rfi)
 
 
+INC_SYNTAX = re.compile(r"{mermaid!\s*(.+?)\s*!((\blines\b)=([0-9 -]+))?\}")
+HEADING_SYNTAX = re.compile("^#+")
+
+PRE_MERMAID = "<div class=mermaid>\n" # "<!-- mermaid start. Do not delete this comment-->\n```mermaid"
+AFTER_MERMAID = "</div>\n" #"```\n<!-- mermaid end. comment needed to it covert to HTML-->"
+
+def process_mermaidInclude(lines, base_path, encoding = "utf-8"):
+    # lines = inputText.splitlines()
+    done = False
+    bonusHeading = ""
+    while not done:
+        for loc, line in enumerate(lines):
+            m = INC_SYNTAX.search(line)
+
+            while m:
+                filename = m.group(1)
+                filename = os.path.expanduser(filename)
+                if not os.path.isabs(filename):
+                    filename = os.path.normpath(
+                        os.path.join(base_path.__str__(), filename)
+                    )
+                try:
+                    with open(filename, "r", encoding=encoding) as r:
+                        original_text = process_mermaidInclude(r.readlines(), base_path, encoding)
+
+                except Exception as e:
+                    # if not self.throwException:
+                    print(
+                        "Warning: could not find file {}. Ignoring "
+                        " try using --assetDir option \n"
+                        "include statement. Error: {}".format(filename, e)
+                    )
+                    lines[loc] = INC_SYNTAX.sub("", line)
+                    break
+                    # else:
+                    #     raise e
+                # if m.group(2) is None:
+                text = original_text
+                # else:
+                #     lines_str = m.group(4)
+                #     lines_blocks = lines_str.split()
+                #     wanted_lines = []
+                #     for block in lines_blocks:
+                #         if "-" in block:
+                #             start, end = block.strip().split("-")
+                #             current_start = int(start)
+                #             current_end = int(end)
+                #             if not len(original_text) >= current_end:
+                #                 current_end = len(original_text)
+                #                 print(
+                #                     f"Warning: line range: {block} ending in "
+                #                     f"line: {end} is larger than file: {filename} "
+                #                     f"using end: {current_end}"
+                #                 )
+                #             if not current_start <= current_end:
+                #                 current_start = max(current_end - 1, 1)
+                #                 print(
+                #                     f"Warning: in line range: {block} "
+                #                     f"the start line: {start} is not "
+                #                     f"smaller than the end line: {current_end} "
+                #                     f"using start: {current_start}"
+                #                 )
+
+                #             wanted_lines.extend(
+                #                 original_text[current_start - 1 : current_end]
+                #             )
+                #         else:
+                #             wanted_line = int(block.strip())
+                #             current_line = wanted_line
+                #             if current_line > len(original_text):
+                #                 current_line = len(original_text)
+                #                 print(
+                #                     f"Warning: line: {wanted_line} is larger than "
+                #                     f"file: {filename} using end: {current_line}"
+                #                 )
+                #             wanted_lines.append(original_text[current_line - 1])
+                #     text = wanted_lines
+
+                if len(text) == 0:
+                    text.append("")
+                # for i in range(len(text)):
+                #     text[i] = text[i].rstrip("\r\n")
+                # text_to_insert = "\r\n".join(text)
+                # line = line[: m.start()] + text_to_insert.strip() + line[m.end() :]
+                # line = PRE_MERMAID + line + AFTER_MERMAID
+                text.append(AFTER_MERMAID)
+                text.insert(0, PRE_MERMAID)
+                del lines[loc]
+                lines[loc:loc] =text #line.splitlines()
+                # #this look like a recursive include tag processing
+                m = INC_SYNTAX.search("")
+
+        else:
+            done = True
+    return lines
 
 if __name__ == "__main__":
     main()
